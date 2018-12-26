168abad61d39767505d5ad8554118d89781101d0
John <john.d.hutchinson@outlook.com>
2018-12-25 20:26:53 -0600

    added movie list csv files

--- whmXML.py
+++ whmXML.py
@@ -1,16 +1,60 @@
 import bs4 as bs
 import urllib.request
 import pandas as pd
+import csv
+import xml.etree.ElementTree as ET
+files = []
 
-sauce = urllib.request.urlopen('http://www.omdbapi.com/?t=the+happening&y=2008&r=xml&apikey=49f91ae0').read()
-soup = bs.BeautifulSoup(sauce, 'xml')
-output = str(soup)
-print(soup)
-f = open('clue_1985.xml', 'w+')
-f.write(output)
-f.close()
 
-key = '49f91ae0'
+def xml_generator():
+    f = open('movie_list.csv')
+    csv_f = csv.reader(f)
+    key = '49f91ae0'
+    for row in csv_f:
+        try:
+            title = row[0]
+            year = row[1]
+            print(title, year)
+            title = title.replace(" ", "+")
+            sauce = urllib.request.urlopen('http://www.omdbapi.com/?t=' +
+                                           title + '&y=' + year + '&r=xml&apikey=' + key).read()
+            soup = bs.BeautifulSoup(sauce, 'xml')
+            output = str(soup)
+            file_name = title.replace("+", "_")
+            f = open('xml/' + file_name + '_' + year + '.xml', 'w+')
+            f.write(output)
+            f.close()
+            files.append(file_name + "_" + year + '.xml')
+
+        except:
+            print('oops....')
+
+
+def parse():
+    for i in files:
+        tree = ET.parse('xml/' + i)
+        root = tree.getroot()
+
+        movie_data = open('movies.csv', 'w')
+
+        csvwriter = csv.writer(movie_data)
+
+        movie_head = []
+
+        for movie in root.findall('movie'):
+            year = movie.find('year')
+            print(year)
+
+        print('hi!')
+def main():
+    xml_generator()
+    parse()
+    print(files)
+
+
+main()
+
+
 
 
 

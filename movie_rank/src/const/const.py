b08b6ec9d426c97894a6f42cc094b9b48c5c4114
John Hutchinson <john.d.hutchinson@outlook.com>
2023-01-16 11:45:03 -0600

    fixing url

--- movie_rank/src/const/const.py
+++ movie_rank/src/const/const.py
@@ -14,6 +14,5 @@ PROJECT_LOCATION = '/'.join(os.getcwd().split('/')[:-2])
 
 API_URL_BASE = 'https://api.themoviedb.org/3'
 API_MOVIE_SEARCH = f"{API_URL_BASE}/search/movie?api_key={get_key()}&query="
-API_GET_CREDITS = f"""{API_URL_BASE}/movie/@/credits?
-                      api_key={get_key()}&language=en-US"""
+API_GET_CREDITS = f'''{API_URL_BASE}/movie/@/credits?api_key={get_key()}&language=en-US'''
 MAX_CAST_SIZE = 75
